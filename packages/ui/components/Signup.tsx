de792cca7d0ae3fa6beb801f2b5afc2a4493116f
Rajat Ranjan <ranjanrajat380@gmail.com>
2023-08-22 23:42:17 +0530

    modified code

--- packages/ui/components/Signup.tsx
+++ packages/ui/components/Signup.tsx
@@ -1,8 +1,61 @@
+
+import { Card, TextField, Typography ,} from "@mui/material";
+import { Button } from "../Button";
+import {useState} from "react";
+
+
 export function Signup(){
-    return<div>
-        <input type="text" placeholder="email"></input>
-        <input type="text" placeholder="password"></input>
-        <button>Submit</button>
-        
+
+    return <div>
+    <div style={{
+        paddingTop: 150,
+        marginBottom: 10,
+        display: "flex",
+        justifyContent: "center"
+    }}>
+     <Typography variant={"h6"}>
+        Welcome to Coursera. Sign up below
+        </Typography>
     </div>
+<div style={{display: "flex", justifyContent: "center"}}>
+    <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+        <TextField
+            onChange={(event) => {
+                setEmail(event.target.value);
+            }}
+            fullWidth={true}
+            label="Email"
+            variant="outlined"
+        />
+        <br/><br/>
+        <TextField
+            onChange={(e) => {
+                setPassword(e.target.value);
+            }}
+            fullWidth={true}
+            label="Password"
+            variant="outlined"
+            type={"password"}
+        />
+        <br/><br/>
+
+        <Button
+            size={"large"}
+            variant="contained"
+            onClick={async() => {
+                const response = await axios.post(`${BASE_URL}/admin/signup`, {
+                    username: email,
+                    password: password
+                })
+                let data = response.data;
+                localStorage.setItem("token", data.token);
+                // window.location = "/"
+                setUser({userEmail: email, isLoading: false})
+                navigate("/courses")
+            }}
+
+        > Signup</Button>
+    </Card>
+</div>
+</div>
 }
